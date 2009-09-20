2f68459cb1de2907694d73cc944bae67a29a11e1
Lukhnos D. Liu <lukhnos@lithoglyph.com>
2009-09-21 04:18:59 +0800

    Fixes a bug that LiHei Pro got always selected when app launches.
    Polishes the wordings in the English localization.
    
    Adds HiraginoSansGB-W3 and HiraginoSansGB-W6 to the list.

--- TCFailAppDelegate.m
+++ TCFailAppDelegate.m
@@ -53,15 +53,24 @@ NSString *plistFolderPath = @"/System/Library/Frameworks/ApplicationServices.fra
 - (void)addFontWithName:(NSString *)name note:(NSString *)note
 {
 	NSMutableDictionary *fontDictionary = [NSMutableDictionary dictionary];
-	[fontDictionary setValue:[NSNumber numberWithBool:NO] forKey:@"checked"];
+	
+	if (![self.currentSystemFontName length]) {
+		[fontDictionary setValue:[NSNumber numberWithBool:NO] forKey:@"checked"];
+	}
+	else {
+		[fontDictionary setValue:[NSNumber numberWithBool:[name isEqualToString:self.currentSystemFontName]] forKey:@"checked"];
+	}
+	
 	[fontDictionary setValue:name forKey:@"name"];
 	[fontDictionary setValue:note forKey:@"note"];
 	[availableFontArray addObject:fontDictionary];
 }
+
 - (void)awakeFromNib
 {
 	[self.window center];
 }
+
 - (NSString *)tempFilePath
 {
 	BOOL isDir;
@@ -241,7 +250,6 @@ NSString *plistFolderPath = @"/System/Library/Frameworks/ApplicationServices.fra
 				for (NSArray *a in item) {
 					if ([a count] >= 2 && [[a objectAtIndex:0] isEqualToString:@"zh-Hant"]) {
 						self.currentSystemFontName = [a objectAtIndex:1];
-						NSLog(@"name:%@", [a objectAtIndex:1]);
 						[self updatePreview];
 						return;
 					}
@@ -254,7 +262,7 @@ NSString *plistFolderPath = @"/System/Library/Frameworks/ApplicationServices.fra
 - (void)updatePreview
 {
 	CGFloat fontSize = [fontSizeSlider doubleValue];
-	NSString *previewText = [NSString stringWithUTF8String:"中文字體範例請晴睛餉飯食令零翱翔賣讀直值楊揚鄉響饗俞輸妳好"];
+	NSString *previewText = [NSString stringWithUTF8String:"中文字體範例請晴睛餉飯食令零翱翔賣讀直值楊揚，鄉響、饗俞。輸妳好"];
 	if (self.currentSystemFontName) {
 		[currentSystemFontNameLabel setStringValue:NSLocalizedString(self.currentSystemFontName, @"")];
 		NSDictionary *attr = [NSDictionary dictionaryWithObjectsAndKeys:[NSFont fontWithName:self.currentSystemFontName size:fontSize], NSFontAttributeName, nil];
@@ -286,11 +294,15 @@ NSString *plistFolderPath = @"/System/Library/Frameworks/ApplicationServices.fra
 	[self.tableView setRowHeight:25.0];
 	[self retrieveCurrentSystemFontName];
 	
+	NSLog(@"current system font name: %@", self.currentSystemFontName);
+	
 	availableFontArray = [[NSMutableArray alloc] init];
 	[self addFontWithName:@"LiHeiPro" note:@"Default TC font of Mac OS X 10.5"];
 	[self addFontWithName:@"LiGothicMed" note:@"Traditional Chinese font"];
 	[self addFontWithName:@"STXihei" note:@"Simplified Chinese font"];
 	[self addFontWithName:@"STHeiti" note:@"Simplified Chinese font"];
+	[self addFontWithName:@"HiraginoSansGB-W3" note:@"Japanese font with Chinese codepoint coverage"];
+	[self addFontWithName:@"HiraginoSansGB-W6" note:@"Japanese font with Chinese codepoint coverage"];
 	[self addFontWithName:@"HiraKakuPro-W3" note:@"Japanese font"];
 	[self addFontWithName:@"HiraKakuPro-W6" note:@"Japanese font"];
 	[self addFontWithName:@"HiraKakuProN-W3" note:@"Japanese font"];
@@ -298,6 +310,18 @@ NSString *plistFolderPath = @"/System/Library/Frameworks/ApplicationServices.fra
 	[self addFontWithName:@"STHeitiTC-Medium" note:@"Default TC font of Mac OS X 10.6"];
 	[self addFontWithName:@"STHeitiTC-Light" note:@"Default TC font of Mac OS X 10.6"];
 	[self.tableView reloadData];
+	
+
+	NSUInteger selectedRow;
+	for (selectedRow = 0 ; selectedRow < [availableFontArray count] ; selectedRow++) {
+		if ([[[availableFontArray objectAtIndex:selectedRow] valueForKey:@"checked"] boolValue]) {
+			break;
+		}
+	}
+	
+	if (selectedRow < [availableFontArray count]) {
+		[self.tableView selectRowIndexes:[NSIndexSet indexSetWithIndex:selectedRow] byExtendingSelection:NO];
+	}
 	
 	[agreeWindow setDefaultButtonCell:[agreeButton cell]];
 	[NSApp beginSheet:agreeWindow modalForWindow:self.window modalDelegate:nil didEndSelector:NULL contextInfo:NULL];
